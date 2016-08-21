69d2444abaab65d254d0745e344f7643c44be586
Aditya Padala <adityapingani@gmail.com>
2016-08-20 22:49:10 -0400

    Update Sentiment_Analysis_nltk_learning.py

--- Sentiment_Analysis_nltk_learning.py
+++ Sentiment_Analysis_nltk_learning.py
@@ -1,10 +1,10 @@
 import nltk
+
 pos_tweets = [('I love this car', 'positive'),
               ('This view is amazing', 'positive'),
               ('I feel great this morning', 'positive'),
               ('I am so excited about the concert', 'positive'),
               ('He is my best friend', 'positive')]
-              
 neg_tweets = [('I do not like this car', 'negative'),
               ('This view is horrible', 'negative'),
               ('I feel tired this morning', 'negative'),
